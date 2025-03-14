ee6de6d395de417cddad505ab98ff4bdf8807d00
naufalzhafran <latifnaufal7@gmail.com>
2025-03-14 15:14:13 +0700

    feat: setup nextra

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import nextra from "nextra";
 import type { NextConfig } from "next";
+
+const withNextra = nextra({
+  contentDirBasePath: "/docs", // Or even nested e.g. `/docs/advanced`
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withNextra(nextConfig);
